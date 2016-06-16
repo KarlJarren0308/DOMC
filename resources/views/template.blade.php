4434362c11d4c72509cc3d957f93764669626a2e
Karl Macadangdang <karljarren0308@yahoo.com>
2016-06-17 06:09:21 +0800

    Added validations.js script tag

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -6,11 +6,14 @@
     <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Raleway:400,300,600">
     <link rel="stylesheet" href="/css/jquery.dataTables.min.css">
     <link rel="stylesheet" href="/css/skeleton.css">
+    <link rel="stylesheet" href="/css/sweetalert.css">
     <link rel="stylesheet" href="/css/font-awesome.min.css">
     <link rel="stylesheet" href="/css/stylesheet.css">
     <script src="/js/jquery.min.js"></script>
     <script src="/js/jquery.dataTables.min.js"></script>
+    <script src="/js/sweetalert.min.js"></script>
     <script src="/js/script.js"></script>
+    <script src="/js/validations.js"></script>
 @yield('pre_ref')
 </head>
 <body>
